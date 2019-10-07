2ff6508bdaa256ffd37e99234ec5d47a8d96c2ad
Diniden <diniden@outlook.com>
2019-10-07 12:40:17 -0600

    Clean ups

--- index.js
+++ index.js
@@ -47,12 +47,6 @@ module.exports = bundler => {
             transforms: {
               linkScss: {
                 resolve(node) {
-                  console.log(
-                    cwd,
-                    node.tag, node.attrs && node.attrs.href,
-                    node.tag === 'link' && node.attrs && node.attrs.href && node.attrs.href.indexOf('css') === node.attrs.href.length - 3 && node.attrs.href.indexOf('http') < 0
-                  );
-
                   // Convert links to styles
                   const isLink = node.tag === 'link' &&
                     node.attrs &&
@@ -82,7 +76,6 @@ module.exports = bundler => {
                   return undefined;
                 },
                 transform(node, data) {
-                  console.log('TRANSFORMING', node.attrs.href);
                   node.tag = 'style';
                   node.attrs['data-link'] = node.attrs.href || node.attrs['data-link'];
                   delete node.attrs.href;
